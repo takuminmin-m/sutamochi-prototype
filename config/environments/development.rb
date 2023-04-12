d8cf0a58809e3b0916586b959c0cfb4a4d0ce05e
takuminmin-m <takuminmin0718@gmail.com>
2023-04-13 06:26:29 +0900

    use gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Use Gmail for development
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5,
+  }
 end
