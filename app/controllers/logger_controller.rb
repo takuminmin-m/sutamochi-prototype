a631f3c5c3c118354a7253da49fba5d31b766270
takuminmin-m <takuminmin0718@gmail.com>
2023-04-17 22:32:50 +0900

    fix

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -11,7 +11,7 @@ class LoggerController < ApplicationController
     今日も一緒に勉強するように誘ってください。"
     @message = miibo_generate(prompt)
     p @message
-    StudyNotifyMailer.with(user: @user, message: @message).start_study_notify.deliver_later(wait: 1.second)
+    StudyNotifyMailer.with(user: @user, message: @message).start_study_notify.deliver_later(wait: 1.day)
 
     render json: @study_log
   end
